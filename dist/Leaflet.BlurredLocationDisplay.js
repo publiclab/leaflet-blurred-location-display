393c7b12c10886d8cf3e9b0b9b6d5e61e7593ec8
mridulnagpal <mridulnagpal07@gmail.com>
2018-08-08 23:37:59 +0530

    Precision examples

--- dist/Leaflet.BlurredLocationDisplay.js
+++ dist/Leaflet.BlurredLocationDisplay.js
@@ -52,10 +52,35 @@ BlurredLocationDisplay = function BlurredLocationDisplay(options) {
     return blurredLocations;
   }
 
+  function exampleMarkers() {
+
+    function marker_display() {
+      
+      console.log(options.blurredLocation.getPrecision());
+      var markers = {
+        marker_1: [1.5, 10.7, 0],
+        marker_2: [2.5, 9.7, 0],
+        marker_3: [4.7, 13.7, 1],
+        marker_4: [0.4, 7.7, 1],
+        marker_5: [-0.5, 5.7, 2],
+        marker_6: [-3.2, 14.4, 2],
+        marker_7: [-5.6, 15.7, 3],
+        marker_8: [-0.6, 20.6, 3],
+      }
+
+      for(marker in markers) {
+        L.marker([markers[marker][0], markers[marker][1]]).addTo(options.blurredLocation.map);
+      }
+
+    }
+    options.blurredLocation.map.on("zoom", marker_display)
+  }
+
   return {
     getBlurredLocations: getBlurredLocations,
     showPopUp: showPopUp,
     Interface: Interface,
+    exampleMarkers: exampleMarkers,
   }
 }
 
