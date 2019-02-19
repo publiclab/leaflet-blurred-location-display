5a75826dc428981abd168eef473e45d68e021045
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-02-19 13:42:33 +0530

    comments removed and intendation done

--- src/blurredLocationDisplay.js
+++ src/blurredLocationDisplay.js
@@ -84,70 +84,63 @@ BlurredLocationDisplay = function BlurredLocationDisplay(options) {
       return false ;
   }
 
-    var PLmarkers_array = [] ;
-    var locations_markers_array = [] ;
+  var PLmarkers_array = [] ;
+  var locations_markers_array = [] ;
 
-    function removeAllMarkers(markers_array)
-    {
-      for(i in markers_array){
-        console.log(i) ;
-         map.removeLayer(markers_array[i]) ;
-       }
-       markers_array = [] ;
-       markers_array.length = 0 ; 
-       console.log("Removed all markers !") ;
-       return markers_array ; 
+  function removeAllMarkers(markers_array) {
+    for(i in markers_array){
+      map.removeLayer(markers_array[i]) ;
     }
+    markers_array = [] ;
+    markers_array.length = 0 ; 
+    console.log("Removed all markers !") ;
+    return markers_array ; 
+  }
 
-    function fetchPeopleData(isOn)
+  function fetchPeopleData(isOn) {
+    if(isOn === true)
     {
-      if(isOn === true)
-      {
+      var NWlat = map.getBounds().getNorthWest().lat ;
+      var NWlng = map.getBounds().getNorthWest().lng ;
+      var SElat = map.getBounds().getSouthEast().lat ;
+      var SElng = map.getBounds().getSouthEast().lng ;
 
-       var NWlat = map.getBounds().getNorthWest().lat ;
-       var NWlng = map.getBounds().getNorthWest().lng ;
-       var SElat = map.getBounds().getSouthEast().lat ;
-       var SElng = map.getBounds().getSouthEast().lng ;
-
-       people_url = "https://publiclab.org/api/srch/nearbyPeople?nwlat=" + NWlat + "&selat=" + SElat + "&nwlng=" + NWlng + "&selng=" + SElng ; 
+      people_url = "https://publiclab.org/api/srch/nearbyPeople?nwlat=" + NWlat + "&selat=" + SElat + "&nwlng=" + NWlng + "&selng=" + SElng ; 
        
-       $.getJSON(people_url , function (data) {
-           //var layerGroup = L.layerGroup() ;
-           console.log("Data items " , data.items) ;
-           if (!!data.items) {
-               for (i = 0; i < data.items.length; i++) {
-                   var mid = data.items[i].doc_id ;
-                   var url = data.items[i].doc_url;
-                   var latitude = data.items[i].latitude ;
-                   var longitude = data.items[i].longitude ;
-                   var title = data.items[i].doc_title;
-                   var m = L.marker([data.items[i].latitude, data.items[i].longitude], {
-                       title: title
-                   }) ;
-                   if(filterCoordinate(latitude , longitude)){
-                      afterDecimal = latitude.toString().split(".")[1] ;
-                      precision = 0 ; 
-                      if(typeof afterDecimal !== "undefined") {
-                        precision = afterDecimal.length ;
-                      }
-                      m.addTo(map).bindPopup("<a href=" + url + ">" + title + "</a> <br> Precision : " + precision) ;
-                      PLmarkers_array[PLmarkers_array.length] = m ;  
-                   }
-                   //layerGroup.addLayer(m) ;
-               }
-           }
+      $.getJSON(people_url , function (data) {
+        if (!!data.items) {
+          for (i = 0; i < data.items.length; i++) {
+            var mid = data.items[i].doc_id ;
+            var url = data.items[i].doc_url;
+            var latitude = data.items[i].latitude ;
+            var longitude = data.items[i].longitude ;
+            var title = data.items[i].doc_title;
+            var m = L.marker([data.items[i].latitude, data.items[i].longitude], {
+                  title: title
+            }) ;
+            if(filterCoordinate(latitude , longitude)){
+              afterDecimal = latitude.toString().split(".")[1] ;
+              precision = 0 ; 
+              if(typeof afterDecimal !== "undefined") {
+                precision = afterDecimal.length ;
+              }
+              m.addTo(map).bindPopup("<a href=" + url + ">" + title + "</a> <br> Precision : " + precision) ;
+              PLmarkers_array[PLmarkers_array.length] = m ;  
+            }
 
-       });
-      }
+          }
+        }
+      });
     }
+  }
 
-    function fetchLocationData(isOn) {
-      if(isOn === true){
-        for(i=0 ; i < options.locations.length ; i++){
-           var latitude = options.locations[i][0] ; 
-           var longitude = options.locations[i][1] ; 
-           var BlackIcon = new L.Icon.BlackIcon() ;
-           if(filterCoordinate(latitude , longitude)){
+  function fetchLocationData(isOn) {
+    if(isOn === true){
+      for(i=0 ; i < options.locations.length ; i++){
+        var latitude = options.locations[i][0] ; 
+        var longitude = options.locations[i][1] ; 
+        var BlackIcon = new L.Icon.BlackIcon() ;
+        if(filterCoordinate(latitude , longitude)){
               afterDecimal = latitude.toString().split(".")[1] ;
               precision = 0 ; 
               if(typeof afterDecimal !== "undefined") {
@@ -156,55 +149,47 @@ BlurredLocationDisplay = function BlurredLocationDisplay(options) {
               var m = L.marker([latitude, longitude] , {icon: BlackIcon}) ;
               m.addTo(map).bindPopup("Precision : " + precision) ;
               locations_markers_array[locations_markers_array.length] = m ;
-           }
-        } 
-      }
+        }
+      } 
     }
+  }
 
-    function fetchPLpeopleAPI() {
-          map.on('zoomend' , function () {
-             // clear all markers 
-             PLmarkers_array = removeAllMarkers(PLmarkers_array) ;
+  function fetchPLpeopleAPI() {
+    map.on('zoomend' , function () {
+      // clear all markers 
+      PLmarkers_array = removeAllMarkers(PLmarkers_array) ;
+      //we can add more API's in similar way by passing boolean value in options from API .
+      fetchPeopleData(options.PLpeopleAPI) ; 
+      }) ;
 
-             console.log("Zoomed !") ; 
-             //we can add more API's in similar way by passing boolean value in options from API .
-             fetchPeopleData(options.PLpeopleAPI) ; 
+      map.on('moveend' , function () {
+        // clear all markers 
+        PLmarkers_array = removeAllMarkers(PLmarkers_array) ;
+        fetchPeopleData(options.PLpeopleAPI) ; 
+      }) ;
+  }
 
-         }) ;
+  if(options.PLpeopleAPI){
+    fetchPLpeopleAPI() ;
+  }
 
-          map.on('moveend' , function () {
-             // clear all markers 
-             PLmarkers_array = removeAllMarkers(PLmarkers_array) ;
+  function activateParameter_locations() {
+    map.on('zoomend' , function () {
+      // clear all markers 
+      locations_markers_array = removeAllMarkers(locations_markers_array) ;
+      fetchLocationData(true) ; 
+    }) ;
 
-             console.log("Panned !") ; 
-             fetchPeopleData(options.PLpeopleAPI) ; 
-         }) ;
-    }
+    map.on('moveend' , function () {
+      // clear all markers 
+      locations_markers_array = removeAllMarkers(locations_markers_array) ;
+      fetchLocationData(true) ; 
+    }) ;
+  }
 
-    if(options.PLpeopleAPI){
-      fetchPLpeopleAPI() ;
-    }
-
-
-    function activateParameter_locations() {
-        map.on('zoomend' , function () {
-            // clear all markers 
-            locations_markers_array = removeAllMarkers(locations_markers_array) ;
-            fetchLocationData(true) ; 
-
-        }) ;
-
-        map.on('moveend' , function () {
-            // clear all markers 
-            locations_markers_array = removeAllMarkers(locations_markers_array) ;
-            fetchLocationData(true) ; 
-        }) ;
-    }
-
-    if(options.locations.length !== 0)
-    {
+  if(options.locations.length !== 0) {
       activateParameter_locations() ;
-    }
+  }
 
   return {
     removeAllMarkers: removeAllMarkers,
