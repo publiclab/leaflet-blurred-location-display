174de81c5c6208c1ad15be786ad25ce55e17ed36
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-03-08 17:13:13 +0530

    tests added

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -7,9 +7,18 @@ describe("Basic testing", function() {
   });
 
   it("Checks if at zoom level 5 , all 7 markers are shown", function () {
-  //  BlurredLocation1.map.setZoom(5);
-
+    BlurredLocation1.map.setZoom(5);
     expect($("#map1").children()[0].childNodes[3].childNodes.length).toBe(7) ;
+  });
+
+  it("Checks if at zoom level 6 , only 5 markers are shown", function () {
+    BlurredLocation2.map.setZoom(6);
+    expect($("#map2").children()[0].childNodes[3].childNodes.length).toBe(5) ;
+  });
+
+  it("Checks if at zoom level 8 , only 3 markers are shown", function () {
+    BlurredLocation2.map.setZoom(8);
+    expect($("#map2").children()[0].childNodes[3].childNodes.length).toBe(3) ;
   });
 
 });
