a605c7350b990c7a79693429ac356c7bcbfdc11e
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-03-28 21:05:38 +0530

    last test runs now/fixtures are loaded now

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -1,31 +1,32 @@
 describe("Basic testing", function() {
   "use strict";
 
- // var fixture = loadFixtures('example.html');
+  // var fixture = loadFixtures('example.html');
 
   it("Basic Test", function () {
   expect(true).toBe(true);
   });
 
   it("Checks if at zoom level 5 , all 7 markers are shown", function () {
-    loadFixtures('example.html');
+   var fixture = loadFixtures('example.html');
     expect($("#map1").children()[0].childNodes[3].childNodes.length).toBe(7) ;
   });
 
   it("Checks if at zoom level 6 , only 5 markers are shown", function () {
   //  BlurredLocation2.map.setZoom(6);
-    loadFixtures('example.html');
+   var fixture = loadFixtures('example.html');
     expect($("#map2").children()[0].childNodes[3].childNodes.length).toBe(5) ;
   });
 
   it("Checks if at zoom level 8 , only 3 markers are shown", function () {
     //BlurredLocation3.map.setZoom(8);
-   loadFixtures('example.html');
+   var fixture = loadFixtures('example.html');
     expect($("#map3").children()[0].childNodes[3].childNodes.length).toBe(3) ;
   });
 
   it("maps are loaded", function () {
-    expect($("#map1")).toBeInDOM() ;
+    var fixture = loadFixtures('example.html');
+    expect(document.getElementById('map1')).toBeInDOM() ;
     expect($("#map2")).toBeInDOM() ;
     expect($("#map3")).toBeInDOM() ;
   });  
