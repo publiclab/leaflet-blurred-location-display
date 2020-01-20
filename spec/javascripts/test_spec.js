004460b564711f4d3500cecfec20f626430fec3a
Natalie <natalie.stjean@gmail.com>
2020-01-20 14:44:19 -0500

    add google api stub for geocoder

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -1,36 +1,47 @@
-//jasmine.getFixtures().fixturesPath = 'spec/javascripts/fixtures' ;
+var window = new Object();
+var google = new Object();
+window.google = google;
+
+/*** Mock Google Maps JavaScript API ***/
+google = {
+  maps: {
+    Geocoder: function(stringObj, functionToDo) {
+      functionToDo = functionToDo || function() {};
+      functionToDo(response.results, "OK");
+    },
+  }
+};
 
 describe("Basic testing", function() {
 
-  // var fixture = loadFixtures('example.html');
+  var geocoder;
+
+  beforeAll(function() {
+    geocoder = jasmine.createSpyObj('Geocoder', ['geocode']);
+    spyOn(google.maps, 'Geocoder').and.returnValue(geocoder);
+  });
 
   beforeEach(function () {
-      loadFixtures('example.html');
-    });
+    loadFixtures('example.html');
+  });
 
   it("Basic Test", function () {
-  expect(true).toBe(true);
+    expect(true).toBe(true);
   });
 
   it("Checks if at zoom level 5 , all 7 markers are shown", function () {
-  // var fixture = loadFixtures('example.html');
     expect($("#map1").children()[0].childNodes[3].childNodes.length).toBe(7) ;
   });
 
   it("Checks if at zoom level 6 , only 5 markers are shown", function () {
-  //  BlurredLocation2.map.setZoom(6);
-  // var fixture = loadFixtures('example.html');
     expect($("#map2").children()[0].childNodes[3].childNodes.length).toBe(5) ;
   });
 
   it("Checks if at zoom level 8 , only 3 markers are shown", function () {
-    //BlurredLocation3.map.setZoom(8);
-  // var fixture = loadFixtures('example.html');
     expect($("#map3").children()[0].childNodes[3].childNodes.length).toBe(3) ;
   });
 
   it("maps are loaded", function () {
-  //  var fixture = loadFixtures('example.html');
     expect(document.getElementById('map1')).toBeInDOM() ;
     expect($("#map2")).toBeInDOM() ;
     expect($("#map3")).toBeInDOM() ;
